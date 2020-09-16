d577ce9ccdec55e763cfc635f26d7b6ffa518a93
tiansimi <970280628@qq.com>
2020-09-16 11:00:16 +0800

    添加一些注释

--- fixture/test_fxit.py
+++ fixture/test_fxit.py
@@ -1,5 +1,7 @@
 __author__ = 'TQ'
+
 import pytest
+
 
 # 不带参数时默认scope="function"
 @pytest.fixture()
@@ -7,14 +9,17 @@ def login():
     print("输入账号，密码先登录")
 
 
+# 用例1需要登录
 def test_s1(login):
     print("用例1：登录之后其它动作111")
 
 
+# 用例2不需要登录
 def test_s2():  # 不传login
     print("用例2：不需要登录，操作222")
 
 
+# 用例3需要登录
 def test_s3(login):
     print("用例3：登录之后其它动作333")
 
